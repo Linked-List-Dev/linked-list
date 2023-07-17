e3ec3dd4c7b330cc1ed54e5cd120f611cff85d2e
TiaSFlores <tia.simone01@gmail.com>
2023-07-16 19:46:16 -0700

    edit profile

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -9,8 +9,12 @@ import {
   Stack,
   Paper,
   Avatar,
+  Button,
+  TextField,
+  Modal,
 } from "@mui/material";
 import Post from "../../components/Post";
+import EditIcon from "@mui/icons-material/Edit";
 
 const placeholder = [
   {
@@ -69,11 +73,37 @@ function Profile() {
     "https://images.pexels.com/photos/1796730/pexels-photo-1796730.jpeg?cs=srgb&dl=pexels-chait-goli-1796730.jpg&fm=jpg"
   );
 
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [formValues, setFormValues] = useState({
+    name: "",
+    biography: "",
+    jobTitle: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  //
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formValues);
+
+    //TODO update profile stuff and state
+  };
+
   return (
     <div>
       <ThemeProvider theme={AppTheme}>
         <Box sx={{ display: "flex", backgroundColor: "page.secondary" }}>
-          <NavigationSidePanel />
+          <NavigationSidePanel position="fixed" />
 
           <Box
             sx={{
@@ -95,21 +125,44 @@ function Profile() {
                     backgroundImage: `url(${headPhoto})`,
                     backgroundSize: "cover",
                     backgroundPosition: "center",
-                    padding: "2vh 2vw 2vw 2vh",
                     borderRadius: "10px",
                     minHeight: "20vh",
                   }}
                 >
-                  <Avatar
-                    src={profileImage}
-                    sx={{
-                      width: 150,
-                      height: 150,
-                      border: "white 4px solid",
-                      borderColor: "page.main",
-                    }}
-                  />
+                  <Box sx={{ position: "relative" }}>
+                    <Box sx={{ paddingLeft: "2vw", paddingBottom: "2vh" }}>
+                      <Avatar
+                        src={profileImage}
+                        sx={{
+                          width: 150,
+                          height: 150,
+                          border: "white 4px solid",
+                          borderColor: "page.main",
+                        }}
+                      />
+                    </Box>
+
+                    <Button
+                      sx={{
+                        position: "absolute",
+                        bottom: "5px",
+                        right: "5px",
+                        bgcolor: "white",
+                        width: "40px",
+                        height: "40px",
+                      }}
+                      onClick={handleOpen}
+                    >
+                      <EditIcon fontSize="large" sx={{ color: "black" }} />
+                    </Button>
+                  </Box>
                 </Box>
+                <Typography variant="h3" color={"text.main"}>
+                  {userName}
+                </Typography>
+                <Typography variant="h6" color={"text.secondary"}>
+                  {jobTitle}
+                </Typography>
               </Box>
 
               <Box>
@@ -145,6 +198,94 @@ function Profile() {
               </Box>
             </Stack>
           </Box>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              sx={{
+                backgroundColor: "page.main",
+                borderRadius: 5,
+                width: "40vw",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+              }}
+            >
+              <Box paddingTop="3vh" paddingBottom="3vh">
+                <Stack
+                  spacing={5}
+                  direction="row"
+                  alignItems="center"
+                  justifyContent="center"
+                  paddingTop="2vh"
+                  paddingBottom="2vh"
+                >
+                  <form onSubmit={handleSubmit}>
+                    <Stack alignItems={"center"} spacing={3}>
+                      <Typography variant="h3" color="accent.main">
+                        Edit Profile
+                      </Typography>
+                      <TextField
+                        sx={{ width: "30vw" }}
+                        variant="outlined"
+                        name="name"
+                        label="Display Name"
+                        value={formValues.name}
+                        onChange={handleChange}
+                        fullWidth
+                      />
+
+                      <TextField
+                        sx={{ width: "30vw" }}
+                        variant="outlined"
+                        name="jobTitle"
+                        label="Job Title"
+                        value={formValues.jobTitle}
+                        onChange={handleChange}
+                        fullWidth
+                      />
+
+                      <TextField
+                        sx={{ width: "30vw" }}
+                        rows={5}
+                        variant="outlined"
+                        name="biography"
+                        label="Biography"
+                        value={formValues.biography}
+                        onChange={handleChange}
+                        fullWidth
+                      />
+
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        size="large"
+                        sx={{
+                          backgroundColor: "accent.main",
+                          textTransform: "none",
+                          width: "auto",
+                          height: "auto",
+                          "&:hover": {
+                            backgroundColor: "accent.secondary",
+                          },
+                        }}
+                      >
+                        <Typography variant="h4">Save changes</Typography>
+                      </Button>
+                    </Stack>
+                  </form>
+                </Stack>
+              </Box>
+            </Box>
+          </Modal>
         </Box>
       </ThemeProvider>
     </div>
