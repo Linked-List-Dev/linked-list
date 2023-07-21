4fac2e6b5565dcd12e096629baac560497344f33
Artem Kolpakov <artemkolpakov78@gmail.com>
2023-07-20 22:52:45 -0700

    feat: implemented CRUD for comments on posts

--- backend/api/controllers/posts.js
+++ backend/api/controllers/posts.js
@@ -4,8 +4,10 @@ import joi from "joi"
 import User from "../../models/User.js"
 import Feed from "../../models/Feed.js"
 import Post from "../../models/Post.js"
+import Comment from "../../models/Comment.js"
 import { requireAuthentication } from "../../middleware/auth.js"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 dotenv.config()
 
@@ -29,13 +31,14 @@ router.post("/", requireAuthentication, async function (req, res, next) {
 
     try {
         const user = await User.findById(userid)
-        
+
         //create the post iteslf and save it
         const postToCreate = new Post({
             description: description,
             authorName: username,
             authorEmail: email,
-            authorJobTitle: user.jobTitle ? user.jobTitle : 'looking for job'
+            authorJobTitle: user.jobTitle ? user.jobTitle : "looking for job",
+            authorId: userid,
         })
 
         const createdPost = await postToCreate.save()
@@ -53,7 +56,8 @@ router.post("/", requireAuthentication, async function (req, res, next) {
             id: createdPost._id,
             description: createdPost.description,
             author: createdPost.authorName,
-            authorJobTitle: createdPost.authorJobTitle
+            authorJobTitle: createdPost.authorJobTitle,
+            authorId: userid,
         })
     } catch (err) {
         return res.status(500).json({ error: err.message })
@@ -228,14 +232,12 @@ router.post("/like/", requireAuthentication, async function (req, res, next) {
         await feed.save()
         const likedPost = await postToLike.save()
 
-        return res
-            .status(200)
-            .json({
-                message: "Post successfully liked!",
-                postId: likedPost._id,
-                likes: likedPost.likes,
-                dislikes: likedPost.dislikes
-            })
+        return res.status(200).json({
+            message: "Post successfully liked!",
+            postId: likedPost._id,
+            likes: likedPost.likes,
+            dislikes: likedPost.dislikes,
+        })
     } catch (err) {
         return res.status(500).json({ error: err.message })
     }
@@ -289,14 +291,165 @@ router.post("/dislike/", requireAuthentication, async function (req, res, next) 
         await feed.save()
         const dislikedPost = await postToDislike.save()
 
-        return res
-            .status(200)
-            .json({
-                message: "Post successfully disliked!",
-                postId: dislikedPost._id,
-                likes: dislikedPost.likes,
-                dislikes: dislikedPost.dislikes
-            })
+        return res.status(200).json({
+            message: "Post successfully disliked!",
+            postId: dislikedPost._id,
+            likes: dislikedPost.likes,
+            dislikes: dislikedPost.dislikes,
+        })
+    } catch (err) {
+        return res.status(500).json({ error: err.message })
+    }
+})
+
+router.post("/comment/", requireAuthentication, async function (req, res, next) {
+    const userid = req.user._id
+    const username = req.user.name
+    const email = req.user.email
+
+    const { postId, commentContent } = req.body
+
+    try {
+        const postToCommentOn = await Post.findById(postId)
+
+        if (!postToCommentOn) {
+            return res
+                .status(400)
+                .json({ error: "Post to comment on not found!" })
+        }
+
+        // Create a new comment
+        const commentToCreate = new Comment({
+            content: commentContent,
+            authorName: username,
+            authorId: userid,
+        })
+
+        const newComment = await commentToCreate.save()
+
+        // Push the new comment into the post.comments array
+        postToCommentOn.comments.push(newComment)
+
+        // Save the updated post
+        const updatedPost = await postToCommentOn.save()
+
+        const feed = await Feed.findOne({ _id: process.env.FEED_ID })
+        const postIndex = feed.posts.findIndex(
+            (post) => post._id.toString() === postId.toString()
+        )
+        feed.posts[postIndex] = updatedPost
+
+        await feed.save()
+
+        return res.status(200).json({
+            message: "New comment successfully added!",
+            id: newComment._id,
+            post: updatedPost,
+        })
+    } catch (err) {
+        return res.status(500).json({ error: err.message })
+    }
+})
+
+router.put("/comment/:commentid", requireAuthentication, async function (req, res, next) {
+    const userid = req.user._id
+    const username = req.user.name
+    const email = req.user.email
+
+    const commentId = req.params.commentid
+    const { newCommentContent } = req.body
+
+    try {
+        const commentToUpdate = await Comment.findById(commentId)
+
+        if (!commentToUpdate) {
+            return res
+                .status(400)
+                .json({ error: "Comment to update not found!" })
+        }
+
+        if (commentToUpdate.authorId.toString() !== userid.toString()) {
+            return res
+                .status(401)
+                .json({ error: "You can only edit your own comments!" })
+        }
+
+        commentToUpdate.content = newCommentContent
+
+        // Save the updated comment
+        const updatedComment = await commentToUpdate.save();
+
+        // Find the post that contains the updated comment
+        const postContainingComment = await Post.findOne({ "comments._id": commentId })
+        // Update the comment content within the post's comments array
+            const updatedPostComments = postContainingComment.comments.map((comment) =>
+            comment._id.toString() === commentId ? { ...comment, content: updatedComment.content } : comment
+        )
+        postContainingComment.comments = updatedPostComments
+
+        // Save the updated post
+        const updatedPost = await postContainingComment.save()
+
+        const feed = await Feed.findOne({ _id: process.env.FEED_ID })
+        const postIndex = feed.posts.findIndex(
+            (post) => post._id.toString() === postContainingComment._id.toString()
+        )
+        feed.posts[postIndex] = updatedPost
+
+        await feed.save()
+
+        return res.status(200).json({
+            message: "Comment successfully updated!",
+            updatedComment,
+        })
+    } catch (err) {
+        return res.status(500).json({ error: err.message })
+    }
+})
+
+router.delete("/comment/:commentid", requireAuthentication, async function (req, res, next) {
+    const userid = req.user._id
+    const commentId = req.params.commentid
+
+    console.log("commentId", commentId)
+    try {
+        const commentToDelete = await Comment.findById(commentId)
+        
+        if (!commentToDelete) {
+            return res
+                .status(400)
+                .json({ error: "Comment to delete not found!" })
+        }
+
+        if (commentToDelete.authorId.toString() !== userid.toString()) {
+            return res
+                .status(401)
+                .json({ error: "You can only delete your own comments!" })
+        }
+
+        const postContainingComment = await Post.findOne({ "comments._id": commentId });
+
+        const commentIndex = postContainingComment.comments.findIndex(
+            (comment) => comment._id.toString() === commentId.toString()
+        )
+
+        if (commentIndex !== -1) {
+            postContainingComment.comments.splice(commentIndex, 1)
+        }
+
+         // Save the updated post
+         const updatedPost = await postContainingComment.save()
+
+        const feed = await Feed.findOne({ _id: process.env.FEED_ID })
+        const postIndex = feed.posts.findIndex(
+            (post) => post._id.toString() === updatedPost._id.toString()
+        )
+        feed.posts[postIndex] = updatedPost
+        await feed.save()
+
+        await Comment.findByIdAndDelete(commentId)
+
+        res.status(204).end()
     } catch (err) {
         return res.status(500).json({ error: err.message })
     }
