b3d78fa5732c4dd9d0bfc73f45e6d410753ef5ba
TiaSFlores <tia.simone01@gmail.com>
2023-08-07 20:36:19 -0700

    404

--- frontend/src/pages/error/FourOhFour.jsx
+++ frontend/src/pages/error/FourOhFour.jsx
@@ -1,7 +1,54 @@
+import { Box, Button, Stack, ThemeProvider, Typography } from "@mui/material";
 import React from "react";
+import AppTheme from "../../util/Theme";
+import LinkedListLogo from "../../assets/IconLight.svg";
+import { Link } from "react-router-dom";
 
 function FourOhFour() {
-  return <h1> 404 - Page Not Found. </h1>;
+  return (
+    <ThemeProvider theme={AppTheme}>
+      <Box>
+        <Box
+          sx={{ position: "absolute" }}
+          paddingTop={"3vh"}
+          paddingLeft={"3vw"}
+        >
+          <img src={LinkedListLogo} />
+        </Box>
+        <Box
+          height={"100vh"}
+          backgroundColor={"page.secondary"}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Box textAlign={"center"}>
+            <Stack spacing={3}>
+              <Typography variant="h2" color={"accent.secondary"}>
+                Page not found
+              </Typography>
+              <Typography variant="h4">
+                Looks like the page you were looking for does not exist.
+              </Typography>
+              <Button
+                component={Link}
+                to="/feed"
+                variant="outlined"
+                sx={{
+                  color: "accent.main",
+                  borderColor: "accent.main",
+                  mx: "auto",
+                  "&:hover": { borderColor: "accent.secondary" },
+                }}
+              >
+                Back to your feed
+              </Button>
+            </Stack>
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default FourOhFour;
